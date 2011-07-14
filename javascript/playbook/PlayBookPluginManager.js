20e1946e2fda59442651442b396b2e26d5cd8dcf
Ken Wallis <kwallis@rim.com>
2011-07-14 13:11:31 -0700

    Groundwork for the network code

--- javascript/playbook/PlayBookPluginManager.js
+++ javascript/playbook/PlayBookPluginManager.js
@@ -34,9 +34,42 @@ phonegap.PluginManager = (function(webworksPluginManager) {
 		}
 	};
 	
+	networkAPI = {
+		execute: function(action, args, win, fail) {
+			var actionFound = false;
+			/**
+			 * For PlayBooks, we currently only have WiFi connections, so return WiFi if there is
+			 * any access at all.
+			 * TODO: update if/when PlayBook gets other connection types...
+			 */
+			switch(action) {
+				case 'isReachable':
+					var networkStatus = NetworkStatus.NOT_REACHABLE;
+					if (blackberry.system.hasDataCoverage()) {
+						networkStatus = NetworkStatus.REACHABLE_VIA_WIFI_NETWORK;
+					}
+                    return { "status" : 1, "message" : Integer.toString(networkStatus) };
+                    
+                case 'getConnectionInfo':
+                	var connection = Connection.NONE;
+                	var event = "offline";
+                	if (blackberry.system.hasDataCoverage()) {
+						connection = Connection.WIFI;
+						event = "online";
+					}
+                    return { "status" : 1, "message" : {"type":connection, "event":event } };
+                
+				
+				default:
+					fail();						
+			}   
+		}
+	};
+	
     var plugins = {
 		'Camera' : cameraAPI,
-		'Device' : deviceAPI
+		'Device' : deviceAPI,
+		'Network Status' : networkAPI
 	};
 	
 	//Instantiate it
